b18ce33fa119e353bbdb35a8a7954efd08102b80
Ben Liew <bliew@splunk.com>
2023-11-29 17:57:54 +1100

    Add more endpoints to client

--- resilient_client.py
+++ resilient_client.py
@@ -13,6 +13,9 @@ class ResilientClient:
     api_key_secret: Optional[str] = None
 
     _simple_client = None
+    COMMON_CLIENT_KWARGS = {
+        "request_max_retries": 1
+    }
 
     @property
     def simple_client(self):
@@ -22,6 +25,19 @@ class ResilientClient:
 
     def list_incidents(self):
         return self.simple_client.get("/incidents")
+
+    def list_artifcats_for_incident(self, incident_id: str):
+        return self.simple_client.get(f"/incidents/{incident_id}/artifacts")
+
+    def get_artifact(self, incident_id: str, artifact_id: str):
+        return self.simple_client.get(f"/incidents/{incident_id}/artifacts/{artifact_id}")
+
+    def get_incident(self, incident_id: str):
+        return self.simple_client.get(f"/incidents/{incident_id}")
+
+    def create_incident(self, payload: dict):
+        assert all([k in payload for k in ["name", "description", "discovered_date"]])
+        return self.simple_client.post(f"/incidents", payload=payload)
 
     def get_authenticated_client(self):
         if self.api_key_id and self.api_key_secret:
@@ -37,12 +53,12 @@ class ResilientClient:
     # https://ibmresilient.github.io/resilient-python-api/pages/resilient/resilient.html#resilient.co3.SimpleClient.get
     # https://www.ibm.com/support/pages/rest-api
     def get_client_with_api_key(self):
-        client = SimpleClient(org_name=self.org_name, base_url=self.base_url)
+        client = SimpleClient(org_name=self.org_name, base_url=self.base_url, **self.COMMON_CLIENT_KWARGS)
         client.set_api_key(self.api_key_id, self.api_key_secret)
         return client
 
     def get_client_with_credentials(self):
-        client = SimpleClient(org_name=self.org_name, base_url=self.base_url)
+        client = SimpleClient(org_name=self.org_name, base_url=self.base_url, **self.COMMON_CLIENT_KWARGS)
         timeout_seconds = 10
         client.connect(email=self.username, password=self.password, timeout=timeout_seconds)
         return client
--- test/test_resilient_client.py
+++ test/test_resilient_client.py
@@ -35,8 +35,34 @@ def test_client_using_api_key_auth():
     assert len(c.get("/incidents")) > 0
 
 
+def test_get_incident(client_from_env):
+    resp = client_from_env.get_incident("2098")
+    assert resp["id"] == 2098
+    assert resp["name"] == "INC03375277 SIEM Malware"
+
+
 def test_list_incidents(client_from_env):
     resp = client_from_env.list_incidents()
     assert len(resp) > 0
     first_incident = resp[0]
     assert all([k in first_incident for k in ["id", "name", "description", "create_date"]])
+
+
+def test_list_artifacts(client_from_env):
+    resp = client_from_env.list_artifcats_for_incident("2098")
+    assert len(resp) > 0
+    assert resp[0]['id'] == 2
+    assert resp[0]['inc_name'] == 'INC03375277 SIEM Malware'
+
+
+def test_get_artifact(client_from_env):
+    resp = client_from_env.get_artifact(incident_id="2098", artifact_id="2")
+    assert resp['id'] == 2
+    assert resp['inc_name'] == 'INC03375277 SIEM Malware'
+
+
+def test_create_incident(client_from_env):
+    # API key we have seems to have only read-only access
+    resp = client_from_env.create_incident(
+        {"name": "Test", "description": "Test", "discovered_date": "2021-01-01T00:00:00.000Z"})
+    assert resp
